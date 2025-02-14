1ed70a96b12c7bbd255af583d1fb7d5187564805
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2025-02-14 12:53:55 -0300

    ajusta formatação do log de inicialização do servidor para melhor legibilidade

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const app = require("./src/app");
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
